8198bb81aee63b3d94e0a7580e799750561e62a9
irulo <irulo@ROG>
2023-04-20 18:37:11 +0200

    Cuarto cambio

--- Proirune1unai2/src/Holairune.java
+++ Proirune1unai2/src/Holairune.java
@@ -3,5 +3,6 @@ public class Holairune {
  public static void main(String[]args) {
 	 System.out.println("Hola miarma");
 	 System.out.println("¿Qué currículum tiene esta tarántula?");
+	 System.out.println("Te tienen que meter en una máquina de triturar la carne pa' salir guapa");
  }
 }
