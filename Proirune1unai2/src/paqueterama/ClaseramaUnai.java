1e8114c42506137c44d903d221d57c06b710781c
hola <hola@DESKTOP-944QK5S>
2023-04-21 22:17:37 +0200

    imposible

--- Proirune1unai2/src/paqueterama/ClaseramaUnai.java
+++ Proirune1unai2/src/paqueterama/ClaseramaUnai.java
@@ -5,6 +5,7 @@ public class ClaseramaUnai {
 	public static void main(String[] args) {
 		
 		System.out.println("Nueva rama número 1275, parfavah que salga ya!");
+		System.out.println("No hay manera");
 
 	}
 
